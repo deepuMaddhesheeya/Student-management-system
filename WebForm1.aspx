5a7ebdc742410401a36748ab7f327b2a2fb47c2b
Deepu Kumar Maddhesheeya <58387789+deepuMaddhesheeya@users.noreply.github.com>
2025-06-10 15:28:51 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Student_mangement_system.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+        }
+        .auto-style2 {
+            margin-left: 9px;
+        }
+        .auto-style3 {
+            margin-left: 5px;
+        }
+        .auto-style4 {
+            margin-left: 19px;
+        }
+    </style>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>
+        <asp:Label ID="Label1" runat="server" Text="Student management system" CssClass="auto-style1"></asp:Label>
+        </strong>
+        <p>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong>
+            <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:TextBox ID="TextBox1" runat="server" CssClass="auto-style3" PlaceHolder="Email" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Mobile&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox2" runat="server" PlaceHolder="Mobile" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox3" runat="server" PlaceHolder="Name" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Class&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox4" runat="server" PlaceHolder="Class" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox5" runat="server" Type="Date" PlaceHolder="Date" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            <strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fee&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:TextBox ID="TextBox6" runat="server" PlaceHolder="Fee" Height="23px" Width="195px"></asp:TextBox>
+            </strong>
+        </p>
+        <p>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" CssClass="auto-style2" OnClick="Button1_Click" Text="Save Data" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" Text="Get Data" OnClick="Button2_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button3" runat="server" Text="Update Data" OnClick="Button3_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:GridView ID="GridView1" runat="server" CssClass="auto-style4" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                <Columns>
+                   <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                             <asp:Button runat="Server" Text="Delete" CommandName ="Select" ButtonType="Button" onClick="btn_click" />
+                        </ItemTemplate>
+                   </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Student_mangement_system
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string constr;
+
+            // for the connection to 
+            // sql server database
+            SqlConnection conn;
+
+            // Data Source is the name of the 
+            // server on which the database is stored.
+            // The Initial Catalog is used to specify
+            // the name of the database
+            // The UserID and Password are the credentials
+            // required to connect to the database.
+            constr = @"Data Source=103.140.219.50;Initial Catalog=Apico;User ID=sa;Password=Securepass123@";
+
+            conn = new SqlConnection(constr);
+
+            // to open the connection
+            conn.Open();
+
+            // use to perform read and write
+            // operations in the database
+            SqlCommand cmd;
+
+            // data adapter object is use to 
+            // insert, update or delete commands
+            SqlDataAdapter adap = new SqlDataAdapter();
+
+            string sql = "";
+
+            // use the defined sql statement
+            // against our database
+
+
+            sql = "INSERT INTO userrig (email,mobile,company,pass,date,status)VALUES ('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "','" + TextBox4.Text + "','" + TextBox5.Text + "','" + TextBox6.Text + "')";
+
+            // use to execute the sql command so we 
+            // are passing query and connection object
+
+
+            // associate the insert SQL 
+            // command to adapter object
+            adap.InsertCommand = new SqlCommand(sql, conn);
+
+            // use to execute the DML statement against
+            // our database
+            adap.InsertCommand.ExecuteNonQuery();
+
+            // closing all the objects
+
+            conn.Close();
+            getData();
+        }
+        public void getData()
+        {
+            string constr;
+
+            // for the connection to 
+            // sql server database
+            SqlConnection conn;
+
+            // Data Source is the name of the 
+            // server on which the database is stored.
+            // The Initial Catalog is used to specify
+            // the name of the database
+            // The UserID and Password are the credentials
+            // required to connect to the database.
+            constr = @"Data Source=103.140.219.50;Initial Catalog=Apico;User ID=Sa;Password=Securepass123@";
+
+            conn = new SqlConnection(constr);
+
+            // to open the connection
+            conn.Open();
+
+            // use to perform read and write
+            // operations in the database
+            SqlCommand cmd;
+
+            // use to read a row in
+            // table one by one
+            SqlDataReader dreader;
+
+            // to store SQL command and
+            // the output of SQL command
+            string sql;
+
+            // use to fetch rows from demo table
+            sql = "Select * from userrig";
+
+            // to execute the sql statement
+            cmd = new SqlCommand(sql, conn);
+
+            // fetch all the rows 
+            // from the demo table
+            dreader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(dreader);
+            GridView1.DataSource = table;
+            GridView1.DataBind();
+
+            // to close all the objects
+            dreader.Close();
+            cmd.Dispose();
+            conn.Close();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            getData();
+        }
+
+         protected void btn_click(object sender, EventArgs e)
+        {
+            int rowindex = ((GridViewRow)(sender as Control).NamingContainer).RowIndex;
+            long studentid = Convert.ToInt64(GridView1.Rows[rowindex].Cells[2].Text);
+            string constr = @"Data Source=103.140.219.50;Initial Catalog=Apico;User ID=Sa;Password=Securepass123@"; ;
+            SqlConnection conn = new SqlConnection(constr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM userrig WHERE mobile='" + studentid + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            getData();
+        }
+
+          protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            string constr;
+
+            // for the connection to 
+            // sql server database
+            SqlConnection conn;
+
+            // Data Source is the name of the 
+            // server on which the database is stored.
+            // The Initial Catalog is used to specify
+            // the name of the database
+            // The UserID and Password are the credentials
+            // required to connect to the database.
+            constr = @"Data Source=103.140.219.50;Initial Catalog=Apico;User ID=sa;Password=Securepass123@";
+
+            conn = new SqlConnection(constr);
+
+            // to open the connection
+            conn.Open();
+
+            // use to perform read and write
+            // operations in the database
+            SqlCommand cmd;
+
+            // data adapter object is use to 
+            // insert, update or delete commands
+            SqlDataAdapter adap = new SqlDataAdapter();
+
+            string sql = "";
+
+            // use the defined sql statement
+            // against our database
+
+
+            sql = "UPDATE  userrig SET email='" + TextBox1.Text + "',mobile='"+ TextBox2.Text + "',company='"+ TextBox3.Text + "',pass='"+ TextBox4.Text + "',date='"+TextBox5.Text + "',status='" + TextBox6.Text + "' WHERE mobile='"+TextBox2.Text+"'";
+
+            // use to execute the sql command so we 
+            // are passing query and connection object
+
+
+            // associate the insert SQL 
+            // command to adapter object
+            adap.InsertCommand = new SqlCommand(sql, conn);
+
+            // use to execute the DML statement against
+            // our database
+            adap.InsertCommand.ExecuteNonQuery();
+
+            // closing all the objects
+
+            conn.Close();
+            getData();
+        }
+    }
+}
